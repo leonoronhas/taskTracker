32070666112212ebcd0d41bd09ad9afb82d960f4
Leonardo <leosud099@gmail.com>
2020-07-08 18:44:25 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {SafeAreaView, View, StatusBar} from 'react-native';
+import {Button} from 'react-native-elements';
+
+import {LogInView} from './auth/LogInView';
+import {AuthProvider, useAuth} from './auth/AuthProvider';
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <AppBody />
+    </AuthProvider>
+  );
+};
+
+// The AppBody is the main view within the App. If a user is not logged in, it
+// renders the login view. Otherwise, it renders the tasks view. It must be
+// within an AuthProvider.
+function AppBody() {
+  const {user, logOut} = useAuth();
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View>
+          {user == null ? (
+            <LogInView />
+          ) : (
+            <Button onPress={logOut} title="Log Out" />
+          )}
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
+
+export default App;
